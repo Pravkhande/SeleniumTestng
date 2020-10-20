d31abe21a8c8c6f7a03673a533f3ed9e3f4b256c
pravkhande <pravkhande@gmail.com>
2020-10-20 12:16:10 +0530

    support for firefox and environment variable. Change path

--- src/com/automation/TRunner/BaseClass.java
+++ src/com/automation/TRunner/BaseClass.java
@@ -1,20 +1,62 @@
 package com.automation.TRunner;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
 	public WebDriver driver;
 	
   @BeforeMethod
-  public void openChromedriver() {
+  public void openDriver() {
 	    System.out.println("In @BeforeMethod");
-		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-		driver= new ChromeDriver();
-		driver.manage().window().maximize();
+	    String os = System.getProperty("os.name");
+	    String browserEnv = System.getenv("BROWSER");
+		if (browserEnv.isEmpty()) {
+			browserEnv = "Chrome";
+		}
+		
+		switch (browserEnv) {
+		case "Chrome":
+    		DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+    		capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS,true);
+    		System.out.println("opening chromedriver");
+	    	System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+	    	if(os.contentEquals("Mac OS")) {
+	    		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+	    	}
+	    	System.out.println("chrome opened");
+			driver= new ChromeDriver();
+			driver.manage().window().maximize();
+			break;
+		case "Firefox":
+			DesiredCapabilities capabilitie = new DesiredCapabilities();
+			capabilitie.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+			System.out.println("opening firefoxdriver");
+			System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver"); 
+	    	if(os.contentEquals("Mac OS")) {
+	    		System.setProperty("webdriver.gecko.driver", "/usr/local/bin/geckodriver");
+	    	}
+			driver= new FirefoxDriver();
+		    driver.manage().window().maximize();
+			break;
+		default:
+			break;
+		}
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		//return driver; // or other browser drivers
+
   }
 
   @AfterMethod
